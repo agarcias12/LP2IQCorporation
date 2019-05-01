f673e3812018de23554eb4dc28bcedf43cb8dc8e
DanielGuzmanT <jhairguz.t@gmail.com>
2019-05-01 17:45:49 -0500

    dao business, cas and component modified

--- IQGestTecDA/src/pe/edu/pucp/inf/iqgesttec/dao/DAOBussiness.java
+++ IQGestTecDA/src/pe/edu/pucp/inf/iqgesttec/dao/DAOBussiness.java
@@ -1,19 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package pe.edu.pucp.inf.iqgesttec.dao;
+
+import java.util.ArrayList;
 
 import pe.edu.pucp.inf.iqgesttec.model.bean.Bussiness;
 
-/**
- *
- * @author ASUS
- */
 public interface DAOBussiness {
-    int CreateBussiness(Bussiness Bussiness);
-    int ModifyBusiness(int idBussiness, Bussiness Bussiness);
-    String ConsultBussiness(int idBussiness);
-    String ConsultLocals(int idBusiness);
+    Bussiness queryAll();
+    ArrayList<Bussiness> queryById(int id);
 }
--- IQGestTecDA/src/pe/edu/pucp/inf/iqgesttec/dao/DAOCas.java
+++ IQGestTecDA/src/pe/edu/pucp/inf/iqgesttec/dao/DAOCas.java
@@ -1,17 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package pe.edu.pucp.inf.iqgesttec.dao;
+
+import java.util.ArrayList;
 
 import pe.edu.pucp.inf.iqgesttec.model.bean.CAS;
 
-/**
- *
- * @author ASUS
- */
 public interface DAOCas {
-    int CreateCas(CAS cas);
-    int ModifyCas(int idCas, CAS cas);
+    ArrayList<CAS> queryAll();
+    CAS queryById(int id);
 }
