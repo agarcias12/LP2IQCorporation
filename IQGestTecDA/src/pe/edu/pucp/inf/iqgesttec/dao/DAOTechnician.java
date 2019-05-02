fc85aa6143d9986bdbb22d51fb06aa2552e3b949
DanielGuzmanT <jhairguz.t@gmail.com>
2019-05-01 20:23:33 -0500

    daos technician and user changed

--- IQGestTecDA/src/pe/edu/pucp/inf/iqgesttec/dao/DAOTechnician.java
+++ IQGestTecDA/src/pe/edu/pucp/inf/iqgesttec/dao/DAOTechnician.java
@@ -1,21 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package pe.edu.pucp.inf.iqgesttec.dao;
 
 import pe.edu.pucp.inf.iqgesttec.model.bean.Efficiency;
 import pe.edu.pucp.inf.iqgesttec.model.bean.Technician;
 
-/**
- *
- * @author ASUS
- */
 public interface DAOTechnician {
-    void CreateTechnician(Technician technician, int idCas, int idUser);
-    void ModifyTechnician(int idTechnician, Technician technician, int idCas);
-    void ModifyEfficiency(int idTechnician, Efficiency efficiency);
-    void GenerateAgenda();//req.18
-    
+    int insert(Technician technician);
+    int update(Technician technician);
+    int delete(int id);
+
+    ArrayList<Technician> queryAll();
+    Technician queryById(int id);
 }
