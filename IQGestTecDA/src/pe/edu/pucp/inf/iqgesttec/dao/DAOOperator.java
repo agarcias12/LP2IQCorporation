bf643f2f7d2f3be5aebdd4345854f1c018230de6
DanielGuzmanT <jhairguz.t@gmail.com>
2019-05-02 09:34:33 -0500

    DAOOperator contiene ahora funciones de MYSQL Operator antes implementadas

--- IQGestTecDA/src/pe/edu/pucp/inf/iqgesttec/dao/DAOOperator.java
+++ IQGestTecDA/src/pe/edu/pucp/inf/iqgesttec/dao/DAOOperator.java
@@ -4,6 +4,11 @@ package pe.edu.pucp.inf.iqgesttec.dao;
 import pe.edu.pucp.inf.iqgesttec.model.bean.Operator;
 
 public interface DAOOperator {
-    // operator usa getEmployeeData casteando el Operator generado antes de obtener su data
-    Operator queryById(int idOperator);
+    // operator usa getEmployeeData casteando el Operator generado antes de obtener
+    // su data
+    int CreateOperator(Operator operator, int idCas);
+
+    void ModifyOperator(int idOperator, Operator operator, int idCas);
+
+    void ModifyLevel(int idOperator, Level level);
 }
